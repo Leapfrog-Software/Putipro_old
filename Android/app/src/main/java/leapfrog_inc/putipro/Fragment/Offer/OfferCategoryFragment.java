9bab277e7b51bf7a364caf188bbc94375c7079e0
default@email.com <default@email.com>
2018-07-02 22:17:12 +0900

    [Android] スプラッシュ・メイン・仕事依頼画面を作成

---
+++ Android/app/src/main/java/leapfrog_inc/putipro/Fragment/Offer/OfferCategoryFragment.java
@@ -0,0 +1,20 @@
+package leapfrog_inc.putipro.Fragment.Offer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import leapfrog_inc.putipro.Fragment.BaseFragment;
+import leapfrog_inc.putipro.R;
+
+public class OfferCategoryFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+
+        View view = inflater.inflate(R.layout.fragment_offer_category, null);
+
+        return view;
+    }
+}
